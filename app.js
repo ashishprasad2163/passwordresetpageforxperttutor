2d6f3b849695bd975948a4a1269cfebe2ea93140
John Doeashishprasad2163 <ashishprasad2163@gmail.com>
2020-06-23 18:46:59 +0530

    staging_1 commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const result = document.getElementById('result');
+const url = window.location.href;
+const pointer = url.indexOf('%');
+const secret = url.slice(pointer + 1);
+const token = 'Bearer ' + secret;
+
+function myFunction() {
+  var x = document.getElementById('password');
+  if (x.type === 'password') {
+    x.type = 'text';
+  } else {
+    x.type = 'password';
+  }
+}
+
+function sendData(e) {
+  let password = document.getElementById('password').value;
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: token,
+  };
+  axios
+    .post(
+      'http://165.22.219.239:4000/api/users/resetpassword',
+      {
+        newPassword: password,
+        resetLink: token,
+      },
+      {
+        headers: headers,
+      }
+    )
+    .then(function (response) {
+      document.getElementById('submitPassword').reset();
+      console.log(response);
+      var data = response.data.message;
+      console.log('message:', data);
+      console.log(pointer);
+      console.log('secret=', secret);
+      console.log('token', token);
+      // console.log('location.href', window.location.href);
+      // const url = window.location.href;
+      // console.log("url:",url);
+      // console.log(url.indexOf("="));
+      // console.log(url.slice(43));
+      alert(data);
+    })
+    .catch(function (error) {
+      console.log('error', error.message);
+      alert(error);
+    });
+
+  e.preventDefault();
+}
+
+document.getElementById('submitPassword').addEventListener('submit', sendData);
