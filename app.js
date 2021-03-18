1717d5c8cf0233f4b0db1f368a8950ac929fec28
ashishprasad2163 <ashishprasad2163@gmail.com>
2021-03-18 17:13:24 +0530

    ip changed

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function sendData(e) {
 
   axios
     .put(
-      'http://167.71.238.21:4000/api/users/resetpassword',
+      'http://206.189.128.104:4000/api/users/resetpassword',
       {
         newPassword: password,
         resetLink: token,
@@ -46,7 +46,7 @@ function sendData(e) {
       // console.log(url.indexOf("="));
       // console.log(url.slice(43));
       alert(data);
-      window.location.replace('http://167.71.238.21/result.html');
+      window.location.replace('http://206.189.128.104/result.html');
     })
     .catch(function (error) {
       console.log('error', error.message);
